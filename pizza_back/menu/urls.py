d1475a8ebe6ca8dfe45217ef50d7c002ca882ab5
hirundos <dkdus@gmail.com>
2025-09-24 22:32:48 +0900

    erase before code

--- pizza_back/menu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('menu/', views.pizza_list, name='pizza_list'),
-    path('menu/types/', views.pizza_type_list, name='pizza_type_list'),
-    path('menu/get_pizza_id/', views.get_pizza_id, name='get_pizza_id')
-]
--- pizza_back/order/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('order/myorder/', views.myorder_list, name='myorder_list'),
-    path('order/', views.order, name='order'),
-    path('order/branch/', views.get_branch, name='get_branch')
-]
